5659d02caf39f9af8cc1554f5c093de8b6b31479
Konstantin Borovik <github@lab5.ca>
2026-09-13 14:38:50 -0400

    refactor(infra): update azure resource naming conventions

--- infra/main.tf
+++ infra/main.tf
@@ -2,10 +2,10 @@ data "azurerm_client_config" "current" {}
 
 locals {
   resource_token      = substr(md5("${data.azurerm_client_config.current.subscription_id}-${var.environment_name}-${var.location}"), 0, 13)
-  resource_group_name = "rg-credit-policy-${var.environment_name}"
-  storage_name        = "stcp${local.resource_token}"
-  search_name         = "srch-cp-${local.resource_token}"
-  foundry_name        = "aif-cp-${local.resource_token}"
+  resource_group_name = "credit-policy-${var.environment_name}"
+  storage_name        = "creditpolicy${local.resource_token}"
+  search_name         = "credit-policy-${local.resource_token}"
+  foundry_name        = "credit-policy-${local.resource_token}"
   operator_principal_id = (
     var.principal_id != "" ? var.principal_id : data.azurerm_client_config.current.object_id
   )
