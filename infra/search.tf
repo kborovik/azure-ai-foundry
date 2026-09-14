426cb145d5f2c580314b3cb03a6527e2fbf5d7c3
Konstantin Borovik <github@lab5.ca>
2026-09-14 10:43:02 -0400

    chore(infra): update search service name

--- infra/search.tf
+++ infra/search.tf
@@ -1,5 +1,5 @@
 resource "azurerm_search_service" "search" {
-  name                          = "credit-policy-lab5"
+  name                          = "credit-policy-1"
   resource_group_name           = azurerm_resource_group.rg.name
   location                      = azurerm_resource_group.rg.location
   sku                           = "basic"
